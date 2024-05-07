f5e9ec9ca20795709dff1c8921a53918602f0cef
Noan <84435602+Noan07@users.noreply.github.com>
2024-05-07 07:46:18 +0200

    :sparkles: update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,27 +5,6 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__container container">
-
-        {/* <ul className="footer__list">
-          <li>
-            <a href="#about" className="footer__link">
-              About
-            </a>
-          </li>
-
-          <li>
-            <a href="#portfolio" className="footer__link">
-              Projects
-            </a>
-          </li>
-
-          <li>
-            <a href="#testimonials" className="footer__link">
-              Testimonials
-            </a>
-          </li>
-        </ul> */}
-
         <span className="footer__copy">
           Copyright &#169; 2024 by Noan Randon | All rigths reserved
         </span>
