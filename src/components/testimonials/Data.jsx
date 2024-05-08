1ea969628ba4de3fe5ef31dd98498045839c15a3
Noan <84435602+Noan07@users.noreply.github.com>
2024-05-08 10:50:52 +0200

    :sparkles: update testimonials data

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -1,6 +1,4 @@
 import Image1 from "../../assets/testimonial1.png";
-import Image2 from "../../assets/testimonial2.png";
-import Image3 from "../../assets/testimonial3.png";
 
 export const Data = [
   {
@@ -12,14 +10,14 @@ export const Data = [
   },
   {
     id: 2,
-    image: Image2,
+    image: Image1,
     title: "Harry Clinton",
     description:
       "A really good job, all aspects of the project were followed step by step and with good results.",
   },
   {
     id: 3,
-    image: Image3,
+    image: Image1,
     title: "Sara Cill",
     description:
       "A really good job, all aspects of the project were followed step by step and with good results.",
